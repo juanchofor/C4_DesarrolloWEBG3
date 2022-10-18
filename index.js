3927ca6b0bbf882f128a917703a94401804fde3f
hpinzon9 <hpinzon9@gmail.com>
2022-10-17 20:28:42 -0500

    Cambios en archivo js

---
+++ index.js
@@ -0,0 +1,17 @@
+//let numero = 11;
+
+/*
+function esPar(numero)
+{
+    return (numero % 2) == 0;
+}*/
+//if (num1/2)
+ console.log("Estamos iniciando pruebas con JavaScipt node")
+
+
+ let num = 13;
+ if (num % 2 == 0){
+    console.log("El numero ingresado es par")
+ }else{
+    console.log("El numero ingresado es Impar")
+ }
