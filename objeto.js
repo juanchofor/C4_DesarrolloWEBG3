c4f2aefd529edcbd1c55f5a8106e6b70cbceb637
hpinzon9 <hpinzon9@gmail.com>
2022-10-17 20:42:09 -0500

    un nuevo archivo con objeto

---
+++ objeto.js
@@ -0,0 +1,17 @@
+let person = {
+    nombres: "Hector Alonso",
+    apellidos: "Pînzon Otalora",
+    altura: 1.87,
+    edad: 52,
+    hijos:{
+        hijo1:{
+            nombre: "Tania",
+            edad: "32"
+        },
+        hijo2:{
+            nombre: "Noa",
+            edad: "7"
+        }
+    }
+}
+console.log(typeof(person));
